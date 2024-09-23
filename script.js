e5ede370bb568ea81ed1d988bfbb5b7b3539d9ec
Narasimhanaidu <84012731+NAIDUNANI@users.noreply.github.com>
2024-09-23 08:51:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function myshopbutton(){
+    alert("Feature coming soon, stay tuned!")
+}
+function myreadmore(){
+    alert("This feature is not available yet!")
+}
+function myshop(){
+    alert("This section's feature will be live shortly!")
+}
+function myaddcart(){
+    alert("Feature coming soon, stay tuned!")
+}
+function mymailbox(){
+    alert("Feature coming soon, stay tuned!")
+}
+function mysentbutton(){
+    alert("This feature is not available yet!")
+}
